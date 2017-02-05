1678b6c24f5d76d3318e5890e81b26dc21f06b3c
Ryan Nystrom <rnystrom@fb.com>
2017-02-04 19:50:49 -0500

    remove mqtt from hap

--- accessories/Flask_accfactory.js
+++ accessories/Flask_accfactory.js
@@ -4,16 +4,6 @@ var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
 var request = require('sync-request');
 
-console.log('Connecting to MQTT...')
-var mqtt = require('mqtt');
-var options = {
-  port: 1883,
-  host: '10.0.1.17',
-  clientId: 'raspbi'
-};
-var client = mqtt.connect(options);
-console.log('MQTT successfully connected!');
-
 console.log('Fetching devices...')
 var res = request('GET', 'http://localhost:5000/devices.json')
 var json = JSON.parse(res.getBody('utf8'));
@@ -50,11 +40,6 @@ for (var i = 0; i < json.length; i++) {
       request('POST', 'http://localhost:5000/devices/save_state/' + obj.pk, {
         json: { 'state': value ? 1 : 0 }
       });
-
-      console.log('Sending IR sequence to ' + obj.channel);
-      console.log(obj.sequence);
-      client.publish(obj.channel, obj.sequence);
-
       callback();
     })
     .on('get', function(callback) {
